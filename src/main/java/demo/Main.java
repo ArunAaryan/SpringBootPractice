373396f64d00ccef7b0de29a2585e25a72a99478
arunaaryan <rnyadav98@gmail.com>
2022-07-06 22:28:03 +0530

    Java BeanConfig Class Context

--- src/main/java/demo/Main.java
+++ src/main/java/demo/Main.java
@@ -1,12 +1,15 @@
 package demo;
 
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class Main {
     public static void main(String[] args){
-        ApplicationContext context = new ClassPathXmlApplicationContext("spring.xml");
+//        ApplicationContext context = new ClassPathXmlApplicationContext("spring.xml");
+        ApplicationContext context = new AnnotationConfigApplicationContext(BeanConfig.class);
             Doctor d = context.getBean(Doctor.class);
+
             d.assist();
             Nurse n = (Nurse) context.getBean("nurse"); // takes the id from spring.xml
             n.assist();
